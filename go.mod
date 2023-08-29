f6eba6d3f4c065efd561698525a8c3365970890b
Ubeydu <u.keles@gmail.com>
2023-08-29 10:22:31 +0300

    added AddUser function

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/Ubeydu/freeble
 
 go 1.21.0
+
+require (
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/term v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+)
--- s5_dbase/users.go
+++ s5_dbase/users.go
@@ -1,6 +1,11 @@
 package s5_dbase
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func CreateUsersTable(dbase *sql.DB) error {
 	userStmt, err := dbase.Prepare(`
@@ -16,6 +21,23 @@ func CreateUsersTable(dbase *sql.DB) error {
 	_, err = userStmt.Exec()
 	if err != nil {
 		return err
+	}
+	return nil
+}
+
+func AddUser(db *sql.DB, username, pass string) error {
+	phash, err := bcrypt.GenerateFromPassword([]byte(pass), 14)
+	if err != nil {
+		return fmt.Errorf("could not make password for %s, %w", pass, err)
+	}
+
+	stmt, err := db.Prepare(`INSERT INTO users (user_name, pass_hash) VALUES (?, ?)`)
+	if err != nil {
+		return fmt.Errorf("could not prepare to add %s to database : %w", username, err)
+	}
+	_, err = stmt.Exec(username, phash)
+	if err != nil {
+		return fmt.Errorf("could not add %s to database : %w", username, err)
 	}
 	return nil
 }
